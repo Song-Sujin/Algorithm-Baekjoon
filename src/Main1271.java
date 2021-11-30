2d45f1fb5aaf81bd77c774f0f1acc85079959ea3
Song-Sujin <ssj1744@naver.com>
2021-11-30 17:43:17 +0900

    solved 1271

---
+++ src/Main1271.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+public class Main1271 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        System.out.println(a.divide(b));
+        System.out.println(a.remainder(b));
+        sc.close();
+    }
+}
