f99504ab2438356a30fb1eb3a566d9cf29b73b0d
Song-Sujin <ssj1744@naver.com>
2021-11-30 17:04:19 +0900

    solved 1001

---
+++ src/Main1001.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main1001 {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a-b);
+        sc.close();
+    }
+}
